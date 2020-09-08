996691da262bca225258e437127d6800bab10878
Eduardo Parra <helloeduardo@icloud.com>
2020-09-08 12:46:55 -0600

    Add test attributes

--- test/renter_test.rb
+++ test/renter_test.rb
@@ -8,4 +8,11 @@ class RenterTest < Minitest::Test
     assert_instance_of Renter, renter
   end
 
+  def test_it_has_attributes
+    renter = Renter.new("Patrick Star", "4242424242424242")
+
+    assert_equal "Patrick Star", renter.name
+    assert_equal "4242424242424242", renter.credit_card_number
+  end
+
 end
